5c296d7c4831fae8a1bd1ff5364d4a7eacf746ac
sam-uel-ayo <samuelayomide0705@gmail.com>
2025-02-03 12:55:29 +0100

    main task

---
+++ assets/controller.php
@@ -0,0 +1,84 @@
+<?
+namespace Number;
+
+use cUtils\cUtils;
+
+class Number
+{
+    public static function classifyNumber($number)
+    {
+        
+
+        $data =  [
+            "number" => $number,
+            "is_prime" => self::isPrime($number),
+            "is_perfect" => self::isPerfect($number),
+            "properties" => self::getProperties($number),
+            "digit_sum" => self::digitSum($number),
+            "fun_fact" => self::getFunFact($number)
+        ];
+        return json_decode(cUtils::returnData(false, $data, true, 200));
+    
+    }
+
+    
+
+    // Prime Number Check
+    public static function isPrime($number): bool {
+        if ($number < 2) return false;
+        for ($i = 2; $i * $i <= $number; $i++) {
+            if ($number % $i === 0) return false;
+        }
+        return true;
+    }
+
+    // Perfect Number check
+    public static function isPerfect($number): bool {
+        $sum = 0;
+        for ($i = 1; $i < $number; $i++) {
+            if ($number % $i === 0) $sum += $i;
+        }
+        if ($sum !== $number) return false;
+        return true;;
+    }
+
+
+    // Properties
+    public static function getProperties($number) {
+        $properties = array();
+
+        // Check if the number is Armstrong
+        if (self::isArmstrong($number)) {
+            $properties[] = "armstrong";
+        }
+
+        // Check if the number is even or odd
+        $properties[] = ($number % 2 === 0) ? "even" : "odd";
+        return $properties;
+    }
+
+    // Armstrong check
+    public static function isArmstrong($number): bool {
+        $sum = 0;
+        $digits = str_split((string) $number);
+        $power = count($digits);
+
+        foreach ($digits as $digit) {
+            $sum += pow((int)$digit, $power);
+        }
+        return (int)$sum === (int)$number; // Ensure both are integers for comparison
+    }
+
+    // Sum of digits
+    public static function digitSum($number): int {
+        return array_sum(str_split((string) $number));
+    }
+
+    // Get fun fact
+    public static function getFunFact($number): string {
+        $apiUrl = "http://numbersapi.com/" . $number . "/math";
+        $fact = @file_get_contents($apiUrl);
+        return $fact ?: "No fact available.";
+    }
+
+}
---
+++ assets/utils.php
@@ -0,0 +1,63 @@
+<?
+namespace cUtils;
+
+
+class cUtils {
+
+    // Output data to user/ frontend
+    public static function outputData ($status=false, $data, $exit =false, $httpStatus=200) 
+    {
+        if ($data == null) {
+            $data = array();
+        }
+
+        http_response_code($httpStatus);
+        header('Content-Type: application/json');
+        echo json_encode($data, JSON_PRETTY_PRINT);
+
+        foreach (get_defined_vars() as $var) {
+            unset($var);
+        }
+
+        if ($exit == true) {
+            exit();
+        }
+    }
+    // End of method
+
+
+    // return data to be used in program
+    public static function returnData ($status= false, $data=array(), $exit = false, $httpStatus) 
+    {
+        $output = array(
+            'status' => $status,
+            'data' => $data,
+            'status_code' => $httpStatus
+        );
+        return json_encode($output);
+
+        foreach (get_defined_vars() as $var) {
+            unset($var);
+        }
+
+        if ($exit == true) {
+            exit();
+        }
+    }
+    // End of method
+
+
+    // Error Handler
+    public static function errorMessage ($isWhat)// $isWhat - what's wrong with the number
+    {
+        $error = array(
+            'number' => $isWhat,
+            'error' => true // Maybe, maybe not :)
+        );
+
+        return $error;
+    } 
+
+    
+
+}
