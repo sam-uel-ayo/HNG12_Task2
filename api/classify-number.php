bff44608685ebb6e3ae89e33af25b7bf5616b956
sam-uel-ayo <samuelayomide0705@gmail.com>
2025-02-03 12:55:05 +0100

    Endpoint

---
+++ api/classify-number.php
@@ -0,0 +1,15 @@
+<?php
+require '../assets/header.php';
+
+use cUtils\cUtils;
+use Number\Number;
+
+
+if (!isset($_GET['number']) || empty(trim($_GET['number']))) {
+    return cUtils::outputData(false, cUtils::errorMessage("Is not set"), true, 400);
+}
+$number = trim($_GET['number']);
+
+//Logic here
+$classification = Number::classifyNumber($number);
+return cUtils::outputData($classification->status, $classification->data, true, $classification->status_code);
